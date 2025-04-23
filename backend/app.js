ddacd3cb6594a53aad5701910f7fd7f63330f8aa
yashvalid <patilllyashhh@gmail.com>
2025-04-23 16:53:29 +0530

    checks

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('/', (req, res) =>{
+    return res.send("hello world");
+})
 app.use("/users", userRoutes);
 app.use("/skillswap", SkillSwapRoutes);
 app.use("/skill", SkillRoutes);
